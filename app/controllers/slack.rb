fd08211994debc41e21808bd368f0a80b8b61231
github username <github email address>
2018-07-27 19:00:23 -0400

    integrating events

--- app/controllers/slack.rb
+++ app/controllers/slack.rb
@@ -5,7 +5,9 @@ class SlackController < ApplicationController
         url = 'https://hooks.slack.com/services/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'
         data =  {"text":'hello world'}.to_json
         response = RestClient.post(url, data, :content_type => :json)
-        puts response
-       
+        
+        render json: {"challenge": params["challenge"] }
     end
+
+
 end
