c6cf86150ff7c55776447b6c9fc527519ff01192
github username <github email address>
2018-07-27 20:38:09 -0400

    logging params

--- app/controllers/slack.rb
+++ app/controllers/slack.rb
@@ -5,6 +5,7 @@ class SlackController < ApplicationController
         url = 'https://hooks.slack.com/services/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'
         data =  {"text":'hello world'}.to_json
         response = RestClient.post(url, data, :content_type => :json)
+        puts params
     end
 
 
