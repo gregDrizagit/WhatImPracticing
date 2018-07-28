ab381b57265ed287ea0c0167408e5eaec1fdfe7d
github username <github email address>
2018-07-27 23:53:43 -0400

    broke text parseing and response details into respective functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session
+
+  def remind_user
+    
+  end
 end
--- app/controllers/slack.rb
+++ app/controllers/slack.rb
@@ -1,11 +1,36 @@
 class SlackController < ApplicationController
 
-    def helloworld
+    def event_receiver
 
+        response_text = parse_text(params['text'])
+        
+        send_response(response_text)
+        
+    end
+
+    def parse_text(text)
+
+        response_text = ''
+
+        case text
+            when text.includes('add')
+                response_text = 'Okay, how long did you practice today'
+            when text.includes('edit')
+                response_text = 'what do you want to edit'
+            when text.includes('delete')
+                response_text = 'what do you want to delete'
+            else
+                response_text = "I don't know what that means. Say something that I know what it means."
+        end
+
+        response_text
+        
+    end
+
+    def send_response(res)
         url = 'https://hooks.slack.com/services/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'
-        data =  {"text":'hello world'}.to_json
+        data =  res.to_json
         response = RestClient.post(url, data, :content_type => :json)
-        puts params
     end
 
 
