4e60b66302f4d73553cf995c66a8dfc680b1691c
github username <github email address>
2018-07-27 20:23:16 -0400

    changed the webhook address to message public channel after event

--- app/controllers/slack.rb
+++ app/controllers/slack.rb
@@ -2,10 +2,9 @@ class SlackController < ApplicationController
 
     def helloworld
 
-        url = 'https://hooks.slack.com/services/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'
+        url = 'https://hooks.slack.com/services/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'
         data =  {"text":'hello world'}.to_json
         response = RestClient.post(url, data, :content_type => :json)
-        puts params
     end
 
 
