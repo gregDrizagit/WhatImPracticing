792534484119c1e69574e0c3ce3c4d49cf7142c1
github username <github email address>
2018-07-28 15:25:30 -0400

    sending through a button

--- app/controllers/slack.rb
+++ app/controllers/slack.rb
@@ -16,7 +16,8 @@ class SlackController < ApplicationController
 
     
         if downcase_text.include? 'add'
-            response_text = "Cool how long did you practice"
+            response_text = add_dialogue()
+
         elsif downcase_text.include? 'delete'
             response_text ="What do you want to delete"
         elsif downcase_text.include? "edit"
@@ -29,9 +30,60 @@ class SlackController < ApplicationController
         
     end
 
+
+    def add_dialogue
+        dialogue = {
+            "text": "Add practice routine/",
+            "attachments": [
+                {
+                    "fallback": "You are unable to choose a game",
+                    "callback_id": "wopr_game",
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "Add",
+                            "text": "What did you practice today?",
+                            "type": "button",
+                            "value": "Add"
+                        }
+
+                    ]
+                }
+            ]
+        }
+
+        # open_dialogue = {
+        #     "trigger_id": "13345224609.738474920.8088930838d88f008e0",
+        #     "dialog": {
+        #         "callback_id": "ryde-46e2b0",
+        #         "title": "Request a Ride",
+        #         "submit_label": "Request",
+        #         "notify_on_cancel": true,
+        #         "elements": [
+        #             {
+        #                 "type": "text",
+        #                 "label": "Pickup Location",
+        #                 "name": "loc_origin"
+        #             },
+        #             {
+        #                 "type": "text",
+        #                 "label": "Dropoff Location",
+        #                 "name": "loc_destination"
+        #             }
+        #         ]
+        #     }
+        # }
+
+    end
+
+    def add_session()
+        Session.add()
+    end
+
     def send_response(res)
         url = 'https://hooks.slack.com/services/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'
-        data = {text: res}.to_json
+        data = res.to_json
         response = RestClient.post(url, data, :content_type => :json)
     end
 
--- app/models/sessions.rb
+++ app/models/sessions.rb
@@ -1,2 +1,6 @@
-class Sessions < ApplicationRecord 
+class Session < ApplicationRecord 
+
+    def add(data)
+        Session.create()
+    end
 end
