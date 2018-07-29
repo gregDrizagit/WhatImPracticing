ba9ea084b5520f67213a91713ee334f1e554588a
github username <github email address>
2018-07-29 10:45:30 -0400

    playing with headers 16

--- app/controllers/slack.rb
+++ app/controllers/slack.rb
@@ -12,7 +12,7 @@ class SlackController < ApplicationController
             end
 
         elsif params['payload']
-                trigger_id = params['payload'][:trigger_id]
+                trigger_id = params['payload']["trigger_id"]
                 puts trigger_id
                 dialogue = open_add_dialogue(trigger_id)
                 send_dialogue(dialogue)
