f45157b8cfcd88b8956aaa6c2324e6dc4cb9e700
github username <github email address>
2018-07-29 12:39:13 -0400

    trying to get concenrs to work

--- app/controllers/concerns/slackable.rb
+++ app/controllers/concerns/slackable.rb
@@ -58,4 +58,32 @@ module Slackable
         }
     end
 
+    def parse_text(text)
+        
+        response_text = ""
+
+        downcase_text = text.downcase
+
+        if downcase_text.include? 'add'
+            response_text = add_trigger()
+        elsif downcase_text.include? 'delete'
+            response_text ="What do you want to delete"
+        elsif downcase_text.include? "edit"
+            response_text = "what do you want to edit"
+        else
+            response_text = "I don't know what that means. Say something that I know what it means."
+        end
+
+        response_text
+        
+    end
+
+    def add_session()
+        # Session.add()
+    end
+
+    def parse_dialogue
+    end
+
+
 end
--- app/controllers/slack.rb
+++ app/controllers/slack.rb
@@ -1,6 +1,6 @@
 class SlackController < ApplicationController
     include Slackable
-    
+
     def event_receiver
 
         if params['event'] # did we get an event message or a payload
@@ -25,34 +25,7 @@ class SlackController < ApplicationController
         
     end
 
-    def parse_text(text)
-        
-        response_text = ""
-
-        downcase_text = text.downcase
-
-        if downcase_text.include? 'add'
-            response_text = add_trigger()
-        elsif downcase_text.include? 'delete'
-            response_text ="What do you want to delete"
-        elsif downcase_text.include? "edit"
-            response_text = "what do you want to edit"
-        else
-            response_text = "I don't know what that means. Say something that I know what it means."
-        end
-
-        response_text
-        
-    end
-
-  
-    def add_session()
-        # Session.add()
-    end
-
-    def parse_dialogue
-    end
-
+   
     def send_response(res)
         url = 'https://hooks.slack.com/services/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'
         data = res.to_json
@@ -60,7 +33,6 @@ class SlackController < ApplicationController
     end
 
     def send_dialogue(dialogue)
-        puts 'sent dialouge!!!!!!!!!!!!!!!!!!!'
         token = 'Bearer xoxb-406277854996-406582689938-SNKLIUU3erryz00MrZDGdOl3'
         url = 'https://slack.com/api/dialog.open'
         data = dialogue.to_json
