08b6168cbcf101af689a53aab14fc47fa782e159
github username <github email address>
2018-08-03 11:35:13 -0400

    commented out broken line

--- app/controllers/concerns/slackable.rb
+++ app/controllers/concerns/slackable.rb
@@ -281,26 +281,26 @@ module Slackable
 
     
     def parse_session_dialogue(resp)
-       actions = "actions": [
-            {
-                "name": session.id,
-                "text": "View",
-                "type": "button",
-                "value": "View"
-            },
-            {
-                "name": session.id,
-                "text": "Delete",
-                "type": "button",
-                "value": "Delete"
-            },
-            {
-                "name": session.id,
-                "text": "Edit",
-                "type": "button",
-                "value": "Edit"
-            }
-        ]
+    #    actions = "actions": [
+    #         {
+    #             "name": session.id,
+    #             "text": "View",
+    #             "type": "button",
+    #             "value": "View"
+    #         },
+    #         {
+    #             "name": session.id,
+    #             "text": "Delete",
+    #             "type": "button",
+    #             "value": "Delete"
+    #         },
+    #         {
+    #             "name": session.id,
+    #             "text": "Edit",
+    #             "type": "button",
+    #             "value": "Edit"
+    #         }
+    #     ]
     end
 
     def parse_exercise_dialogue(resp)
