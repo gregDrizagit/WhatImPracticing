ee0381fc026d9dcbe331a7d78ac3aca6ec81d349
github username <github email address>
2018-08-07 22:15:27 -0400

    deleted some useless methods

--- app/models/session.rb
+++ app/models/session.rb
@@ -6,23 +6,13 @@ class Session < ApplicationRecord
         @session = Session.create(data)
     end
 
-    def find_and_delete()
-
-    end
-  
     def self.get_last_session
         last = Session.all.last
-    end
-
-    def self.get_today_session
-        # should get session if if the last session's date matches today's date
     end
 
     def self.get_sessions_for_week
         result = Session.all[0..5]
     end
 
-    def self.delete_session
-    end
 
 end
